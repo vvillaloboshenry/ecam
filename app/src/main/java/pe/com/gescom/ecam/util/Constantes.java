053fb065df226c6bbf51be7a7d64e7ead27def2c
Henry Vasquez Villalobos <vvillaloboshenry@gmail.com>
2024-11-25 21:47:30 -0500

    init

---
+++ app/src/main/java/pe/com/gescom/ecam/util/Constantes.java
@@ -0,0 +1,131 @@
+package pe.com.gescom.ecam.util;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.StrictMode;
+import android.view.ContextThemeWrapper;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import pe.com.gescom.ecam.R;
+
+public class Constantes {
+    private Context context;
+    public static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 842;
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 843;
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 8575;
+    public static final int MY_PERMISSIONS_Multiple = 8585;
+    // PERMISOS GLOBAL VARIABLES
+    public static int CODE_REQUEST_PERMISSIONS = 105;
+    public static String[] PERMISSIONS_APP = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_MEDIA_IMAGES};
+
+    public Constantes(Context context) {
+        this.context = context;
+        PolicyAndroid();
+    }
+
+    private void PolicyAndroid() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public ProgressDialog createProgressDialog(String Mensaje) {
+        ProgressDialog progressDialog;
+        progressDialog = new ProgressDialog(new ContextThemeWrapper(context, android.R.style.Theme_Holo_Light_Dialog));
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage(Mensaje);
+        progressDialog.setTitle("AppLecturas");
+        progressDialog.setCanceledOnTouchOutside(false);
+        return progressDialog;
+    }
+
+    public ProgressDialog createProgressDialog1() {
+        ProgressDialog progressDialog;
+        progressDialog = new ProgressDialog(new ContextThemeWrapper(context, android.R.style.Theme_Holo_Light_Dialog));
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setMessage("Enviando Datos...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        return progressDialog;
+    }
+
+    public void Mensaje(String mensaje) {
+        Dialog dialogCarga;
+        DialogAvisos.Builder CustomDialog = new DialogAvisos.Builder(context);
+        CustomDialog.setMessage(mensaje).setTitle("AppLecturas");
+        dialogCarga = CustomDialog.create();
+        if (!((Activity) context).isFinishing()) {
+            dialogCarga.show();
+            dialogCarga.setCanceledOnTouchOutside(true);
+        }
+    }
+
+    public void Mensaje(String title, String mensaje) {
+        Dialog dialogCarga;
+        DialogAvisos.Builder CustomDialog = new DialogAvisos.Builder(context);
+        CustomDialog.setMessage(mensaje).setTitle(title);
+        dialogCarga = CustomDialog.create();
+        if (!((Activity) context).isFinishing()) {
+            dialogCarga.show();
+            dialogCarga.setCanceledOnTouchOutside(true);
+        }
+    }
+
+    public void MensajeBloqueo(String mensaje) {
+        Dialog dialogCarga;
+        DialogAvisos.Builder CustomDialog = new DialogAvisos.Builder(context);
+        CustomDialog.setMessage(mensaje).setTitle("AppLecturas");
+        dialogCarga = CustomDialog.create();
+        if (!((Activity) context).isFinishing()) {
+            dialogCarga.show();
+            dialogCarga.setCanceledOnTouchOutside(false);
+        }
+    }
+
+    public String fechaActual() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        return df3.format(c.getTime());
+    }
+
+    public void GrabarSharedPreferences(SharedPreferences sharedPreferences, String key, String mensaje) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, mensaje);
+        editor.apply();
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void MostrarDialog(String mensaje, DialogInterface.OnClickListener onClickListenerPositive, DialogInterface.OnClickListener onClickListenerNegative) {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(context);
+        mBuilder.setTitle(context.getString(R.string.app_name));
+        mBuilder.setMessage(mensaje);
+        mBuilder.setPositiveButton("ok", onClickListenerPositive);
+        mBuilder.setNegativeButton("no", onClickListenerNegative);
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+}
---
+++ app/src/main/java/pe/com/gescom/ecam/view/CameraManager.java
@@ -0,0 +1,238 @@
+package pe.com.gescom.ecam.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+
+public class CameraManager {
+    private static CameraManager instance;
+    public static final int QUALITY_DEFAULT = 100;
+    public static final ReturnType RETURNTYPE_DEFAULT = ReturnType.BITMAP;
+
+    public enum ReturnType {
+        FILE_PATH, BASE64, FILE, BITMAP
+    }
+
+    public enum Quality {
+        NORMAL, BUENA, MUY_BUENA
+    }
+
+    private WeakReference<AppCompatActivity> activityRef;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    private Object callback;
+    private int quality = QUALITY_DEFAULT;
+    private ReturnType returnType = RETURNTYPE_DEFAULT;
+
+    private CameraManager() {
+    }
+
+    private void setActivity(AppCompatActivity activity) {
+        this.activityRef = new WeakReference<>(activity);
+    }
+
+    private void setActivityResultLauncher(ActivityResultLauncher<Intent> launcher) {
+        this.activityResultLauncher = launcher;
+    }
+
+    /**
+     * Inicializar CameraManager
+     *
+     * @param activity
+     * @return
+     */
+    public static CameraManager with(AppCompatActivity activity) {
+        return getInstance(activity);
+    }
+
+    /**
+     * Metodo estático para obtener o crear una instancia de CameraManager.
+     *
+     * @param activity
+     * @return
+     */
+    public static synchronized CameraManager getInstance(AppCompatActivity activity) {
+        if (instance == null) {
+            instance = new CameraManager();
+            ActivityResultLauncher<Intent> launcher = activity.registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> instance.handleResult(result)
+            );
+            instance.setActivity(activity);
+            instance.setActivityResultLauncher(launcher);
+        } else if (instance.activityRef.get() != activity) {
+            if (activity.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+                throw new IllegalStateException("El contexto debe configurarse antes de que la actividad esté STARTED o RESUMED.");
+            }
+            ActivityResultLauncher<Intent> launcher = activity.registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> instance.handleResult(result)
+            );
+            instance.setActivity(activity);
+            instance.setActivityResultLauncher(launcher);
+        }
+        return instance;
+    }
+
+    public static synchronized CameraManager get() {
+        return instance;
+    }
+
+    public CameraManager quality(int quality) {
+        this.quality = quality;
+        return this;
+    }
+
+    public CameraManager resultType(ReturnType returnType) {
+        this.returnType = returnType;
+        return this;
+    }
+
+    public CameraManager result(CallbackBase64 callback) {
+        this.callback = callback;
+        return this;
+    }
+
+    public CameraManager result(CallbackBitmap callback) {
+        this.callback = callback;
+        return this;
+    }
+
+    public CameraManager result(CallbackFile callback) {
+        this.callback = callback;
+        return this;
+    }
+
+    public CameraManager result(CallbackFilePath callback) {
+        this.callback = callback;
+        return this;
+    }
+
+    /**
+     * Metodo para iniciar la cámara.
+     */
+    public void start() {
+        AppCompatActivity activity = activityRef.get();
+        if (activity == null) {
+            throw new IllegalStateException("La actividad no está configurada o ha sido destruida.");
+        }
+        if (activityResultLauncher == null) {
+            throw new IllegalStateException("CameraManager.activityResultLauncher no está inicializado correctamente.");
+        }
+        Intent cameraIntent = new Intent(activity, CamaraActivity.class);
+        cameraIntent.putExtra("quality", quality);
+        cameraIntent.putExtra("returnType", returnType.name());
+        activityResultLauncher.launch(cameraIntent);
+        activity.overridePendingTransition(0, 0);
+    }
+
+    protected void handleResult(ActivityResult activityResult) {
+        Intent data = activityResult.getData();
+        int resultCode = activityResult.getResultCode();
+        if (resultCode == Activity.RESULT_OK && data != null) {
+
+            try {
+                Object resultData = switch (returnType) {
+                    case BITMAP -> data.getParcelableExtra("bitmap");
+                    case BASE64 -> data.getStringExtra("base64");
+                    case FILE -> data.getStringExtra("file");
+                    case FILE_PATH -> data.getStringExtra("filePath");
+                };
+
+                if (callback != null) {
+                    if (returnType == ReturnType.BITMAP && callback instanceof CallbackBitmap && resultData instanceof Bitmap) {
+                        ((CallbackBitmap) callback).onSuccess((Bitmap) resultData);
+                    }
+                    if (returnType == ReturnType.BASE64 && callback instanceof CallbackBase64 && resultData instanceof String) {
+                        ((CallbackBase64) callback).onSuccess((String) resultData);
+                    }
+                    if (returnType == ReturnType.FILE && callback instanceof CallbackFile && resultData instanceof File) {
+                        ((CallbackFile) callback).onSuccess((File) resultData);
+                    }
+                    if (returnType == ReturnType.FILE_PATH && callback instanceof CallbackFilePath && resultData instanceof String) {
+                        ((CallbackFilePath) callback).onSuccess((String) resultData);
+                    }
+                }
+            } catch (Exception e) {
+                if (callback != null) {
+                    if (callback instanceof CallbackBitmap) {
+                        ((CallbackBitmap) callback).onError(e);
+                    }
+                    if (callback instanceof CallbackBase64) {
+                        ((CallbackBase64) callback).onError(e);
+                    }
+                    if (callback instanceof CallbackFile) {
+                        ((CallbackFile) callback).onError(e);
+                    }
+                    if (callback instanceof CallbackFilePath) {
+                        ((CallbackFilePath) callback).onError(e);
+                    }
+                }
+            }
+        } else if (resultCode != Activity.RESULT_OK) {
+            if (callback != null) {
+                if (callback instanceof CallbackBitmap) {
+                    ((CallbackBitmap) callback).onCanceled("Operacion cancelada");
+                }
+                if (callback instanceof CallbackBase64) {
+                    ((CallbackBase64) callback).onCanceled("Operacion cancelada");
+                }
+                if (callback instanceof CallbackFile) {
+                    ((CallbackFile) callback).onCanceled("Operacion cancelada");
+                }
+                if (callback instanceof CallbackFilePath) {
+                    ((CallbackFilePath) callback).onCanceled("Operacion cancelada");
+                }
+            }
+        }
+    }
+
+    public void release() {
+        if (activityRef != null) {
+            activityRef.clear(); // Elimina la referencia débil
+        }
+        instance = null;
+        this.activityResultLauncher = null; // Libera recursos adicionales
+        this.callback = null;
+    }
+
+    public interface CallbackBitmap {
+        void onSuccess(Bitmap result);
+
+        void onCanceled(String message);
+
+        void onError(Exception e);
+    }
+
+    public interface CallbackBase64 {
+        void onSuccess(String result);
+
+        void onCanceled(String message);
+
+        void onError(Exception e);
+    }
+
+    public interface CallbackFile {
+        void onSuccess(File result);
+
+        void onCanceled(String message);
+
+        void onError(Exception e);
+    }
+
+    public interface CallbackFilePath {
+        void onSuccess(String result);
+
+        void onCanceled(String message);
+
+        void onError(Exception e);
+    }
+}
